edf4195cf5cc15352ddef210f402ebb455164ff7
porosh2002 <jamilkashem2002@gmail.com>
2020-12-17 02:02:40 +0600

    pagination controller color schema updated

---
+++ JavaScript/main.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    center:true,
+    responsive:{
+        600:{
+            items:1
+        },
+        900:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
