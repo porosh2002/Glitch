d2a22df9310c439132c897591ab546d01cdddf69
porosh2002 <jamilkashem2002@gmail.com>
2020-12-19 21:14:29 +0600

    short search functionality added

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,4 +1,12 @@
-const BTN = document.getElementById('Search')
+const BTN = document.getElementById('SearchBTN');
+const ShortSearch = document.getElementById('Search');
+const ShortSearchBTN = document.getElementById('SearchBTN_');
+BTN.addEventListener('click', () => {
+    ShortSearch.classList.toggle('None')
+})
+ShortSearchBTN.addEventListener('click', () => {
+    ShortSearch.classList.toggle('None')
+})
 $('.owl-carousel').owlCarousel({
     loop: true,
     center:true,
