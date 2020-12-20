28bb1032bfb20798fda482ecfeaa949ea831b9cc
porosh2002 <jamilkashem2002@gmail.com>
2020-12-20 22:43:57 +0600

    slider updated

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -11,11 +11,8 @@ $('.owl-carousel').owlCarousel({
     loop: true,
     center:true,
     responsive:{
-        600:{
-            items:1
-        },
         900:{
-            items:2
+            items:1
         },
         1000:{
             items:3
