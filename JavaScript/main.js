3745e5f2c15c0d82f21c0d7af4c03fa109d7c963
porosh2002 <jamilkashem2002@gmail.com>
2020-12-21 21:08:02 +0600

    menu for mobile 1.0.0

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,25 +1,25 @@
-const BTN = document.getElementById('SearchBTN');
-const ShortSearch = document.getElementById('Search');
-const ShortSearch_Mobile = document.getElementById('mobileSEarchIcon');
-const ShortSearchBTN = document.getElementById('SearchBTN_');
-BTN.addEventListener('click', () => {
-    ShortSearch.classList.toggle('None')
-})
-ShortSearchBTN.addEventListener('click', () => {
-    ShortSearch.classList.toggle('None')
-})
-ShortSearch_Mobile.addEventListener('click', () => {
-    ShortSearch.classList.toggle('None')
-})
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    center:true,
-    responsive:{
-        300:{
-            items:1
-        },
-        900:{
-            items:3
-        }
-    }
-})
+const BTN = document.getElementById("SearchBTN");
+const ShortSearch = document.getElementById("Search");
+const ShortSearch_Mobile = document.getElementById("mobileSEarchIcon");
+const ShortSearchBTN = document.getElementById("SearchBTN_");
+BTN.addEventListener("click", () => {
+  ShortSearch.classList.toggle("None");
+});
+ShortSearchBTN.addEventListener("click", () => {
+  ShortSearch.classList.toggle("None");
+});
+ShortSearch_Mobile.addEventListener("click", () => {
+  ShortSearch.classList.toggle("None");
+});
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  center: true,
+  responsive: {
+    300: {
+      items: 1,
+    },
+    900: {
+      items: 3,
+    },
+  },
+});
