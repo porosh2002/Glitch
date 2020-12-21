4c179a369c37dfeb6f75bc31ce2eae4461896837
porosh2002 <jamilkashem2002@gmail.com>
2020-12-21 22:04:29 +0600

    3.0.0

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,7 +1,23 @@
 const BTN = document.getElementById("SearchBTN");
 const ShortSearch = document.getElementById("Search");
 const ShortSearch_Mobile = document.getElementById("mobileSEarchIcon");
+const Menu_sub = document.getElementById("menu_sub")
+const Menu_sub_content = document.getElementById("menu_sub_content")
 const ShortSearchBTN = document.getElementById("SearchBTN_");
+const MobileMenu = document.getElementById("mobileMenu");
+const BarMobile = document.getElementById("BarMobile");
+const TimesMenu = document.getElementById("TimesMenu");
+BarMobile.addEventListener("click", () => {
+  MobileMenu.classList.toggle("None");
+});
+TimesMenu.addEventListener('click', () => {
+  MobileMenu.classList.toggle("None");
+})
+// 
+Menu_sub.addEventListener("click", () => {
+  Menu_sub_content.classList.toggle("None");
+});
+// 
 BTN.addEventListener("click", () => {
   ShortSearch.classList.toggle("None");
 });
