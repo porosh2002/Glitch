2af04ebd633f35f77ca45cbfa9241f1cbee76e23
porosh2002 <jamilkashem2002@gmail.com>
2020-12-21 20:27:52 +0600

    search short for mobile nworking

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,15 +1,14 @@
 const BTN = document.getElementById('SearchBTN');
-const searchMobile = document.getElementById('searchMobile');
 const ShortSearch = document.getElementById('Search');
+const ShortSearch_Mobile = document.getElementById('mobileSEarchIcon');
 const ShortSearchBTN = document.getElementById('SearchBTN_');
 BTN.addEventListener('click', () => {
     ShortSearch.classList.toggle('None')
 })
-// searchMobile.addEventListener('click', () => {
-//     // console.log('clicked');
-//     ShortSearch.classList.toggle('None')
-// })
 ShortSearchBTN.addEventListener('click', () => {
+    ShortSearch.classList.toggle('None')
+})
+ShortSearch_Mobile.addEventListener('click', () => {
     ShortSearch.classList.toggle('None')
 })
 $('.owl-carousel').owlCarousel({
